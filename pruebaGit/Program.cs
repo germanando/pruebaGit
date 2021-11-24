780750ca6405cfe5e73026e1628b1abf91004597
GAD <gad@informatica.euroval.com>
2021-11-24 09:43:56 +0100

    2

--- pruebaGit/Program.cs
+++ pruebaGit/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-for (int i = 0; i < 3; i++)
+for (int i = 0; i < 4; i++)
 {
     Console.WriteLine("Hello, World!");
 }
