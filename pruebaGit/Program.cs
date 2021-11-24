e3468b81b7ac142889513664615089fc510c37e9
GAD <gad@informatica.euroval.com>
2021-11-24 17:38:41 +0100

    cambios importantes en dev

--- pruebaGit/Program.cs
+++ pruebaGit/Program.cs
@@ -4,6 +4,6 @@ for (int i = 0; i < 77; i++)
     Console.WriteLine("Hello, World!");
     Console.WriteLine("Hello, World!");
     
-    //mergeo desde master
+    //cambios importantes en dev
 }
 //comentario
