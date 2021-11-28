1dccb5077ae7648a2e3e694b7d5c8853fea754a1
GAD <gad@informatica.euroval.com>
2021-11-28 09:04:23 +0100

    cambio feature 2

--- pruebaGit/Program.cs
+++ pruebaGit/Program.cs
@@ -5,6 +5,6 @@ for (int i = 0; i < 77; i++)
     Console.WriteLine("Hello, World!");
 
     //cambios importantes en dev
-    Console.WriteLine("Feature 2 !");
+    Console.WriteLine("Feature 2 !"); 
 }
 //comentario
