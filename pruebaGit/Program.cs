d81603d834fa6a7310c119e0e4113eb87f31c795
GAD <gad@informatica.euroval.com>
2021-12-01 14:19:17 +0100

    feature1

--- pruebaGit/Program.cs
+++ pruebaGit/Program.cs
@@ -5,7 +5,7 @@ for (int i = 0; i < 77; i++)
 {
     Console.WriteLine("Hello, World!");
     Console.WriteLine("Hello, World!");
-    Console.WriteLine("Feature 1.1!"); 
+    Console.WriteLine("Feature 1.1!");  
 
     //cambios importantes en dev
 }
